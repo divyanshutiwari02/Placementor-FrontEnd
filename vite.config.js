abf28111f63ecf4e95f8448fd541b323c55c8c02
divyanshutiwari02 <divyanshu.tiwari02@gmail.com>
2026-07-18 22:21:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base:"/Placementor-FrontEnd/",
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'https://place-d7ee.onrender.com',
+      "/api": {
+        target: "https://place-d7ee.onrender.com",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
